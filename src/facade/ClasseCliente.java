4ae8f5f8f3e698a12c2e1ea565b3dae2caf08406
Rodolpho Erick <rodolphoerick90@gmail.com>
2018-11-30 08:44:43 -0300

    Implementação da classe de testes ClasseCliente.java

--- src/facade/ClasseCliente.java
+++ src/facade/ClasseCliente.java
@@ -1,9 +1,27 @@
 package facade;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class ClasseCliente {
+	private ComputadorFacade computador;
+	private CPU cpu = null;
+	private Memoria memoria = null;
+	private HardDrive hd = null;
 
+	@Before
+	public void setUp() {
+		cpu = new CPU();
+		memoria = new Memoria();
+		hd = new HardDrive();
+		computador = new ComputadorFacade(cpu, memoria, hd);
+	}
+	
+	@Test
+	public void test_ligar_computador() {
+		computador.ligarComputador();
+	}
 }
